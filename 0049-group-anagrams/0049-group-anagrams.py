c2301391cd5303953b67188849619b5307f44101
Patneedi369 <Patneedi369@gmail.com>
2026-06-20 14:10:57 +0000

    Solved Group Anagrams

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        groups = {}
+        for i in strs:
+            count = [0] * 26
+            for ch in i:
+                index = ord(ch) - ord('a')
+                count[index] += 1
+
+            key = tuple(count)
+            if key not in groups:
+                groups[key] = []
+            groups[key].append(i)
+
+        return list(groups.values())
