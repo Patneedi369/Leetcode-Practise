7917fcba9e8281f14687e3ab923cf00eae44c9e9
Patneedi369 <Patneedi369@gmail.com>
2026-06-26 10:07:40 +0000

    Solved 3Sum

---
+++ 0015-3sum/0015-3sum.py
@@ -0,0 +1,34 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        res = []
+        
+        for i in range(len(nums)):
+            # Skip the same first element to avoid duplicate triplets
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+                
+            left = i + 1
+            right = len(nums) - 1
+            target = -nums[i]
+            
+            while left < right:
+                current_sum = nums[left] + nums[right]
+                
+                if current_sum == target:
+                    res.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    
+                    # Skip duplicates for left and right pointers
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                        
+                elif current_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+                    
+        return res
