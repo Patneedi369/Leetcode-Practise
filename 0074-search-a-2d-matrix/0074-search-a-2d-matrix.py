66bde2ab0108996c69eb82fff4e22682cfa3f3d4
Patneedi369 <patneedi369@gmail.com>
2026-07-05 22:37:49 +0530

    Time: 0 ms (100%), Space: 19.4 MB (96.64%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,35 +1,19 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        row_size = len(matrix)
-        column_size = len(matrix[0])
+        rows = len(matrix)
+        cols = len(matrix[0])
 
-        top = 0
-        bottom = row_size - 1
-
-        while top <= bottom:
-            mid = (top+bottom)//2
-
-            if target > matrix[mid][-1]:
-                top = mid + 1
-            elif target < matrix[mid][0]:
-                bottom = mid - 1
-            else:
-                top = mid
-                break
-
-        # If top moved past the last row, the target is too large to exist in the matrix
-        if top >= row_size:
-            return False
-            
         left = 0
-        right = column_size - 1
+        right = (rows*cols) - 1
 
         while left <= right:
             mid = (left + right)//2
 
-            if target == matrix[top][mid]:
+            mid_element = matrix[mid//cols][mid%cols]
+            
+            if target == mid_element:
                 return True
-            if target > matrix[top][mid]:
+            if target > mid_element:
                 left = mid + 1
             else:
                 right = mid - 1
