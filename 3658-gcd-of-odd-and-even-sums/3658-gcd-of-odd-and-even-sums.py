aa99d68b3b25e66bfe6bcaefcf5fbd5e54ec8746
Patneedi369 <patneedi369@gmail.com>
2026-07-15 21:44:53 +0530

    Time: 0 ms (100%), Space: 19.1 MB (85.77%) - LeetHub

---
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.py
@@ -0,0 +1,5 @@
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        return n
+
+        # The mathematical sum of the first n odd numbers is n² and the first n even numbers is n(n+1). Their greatest common divisor is always n. Therefore, you just need to return the integer n itself. This takes O(1) time and space.
