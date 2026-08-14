50700d74d71508c7624bc4f4a4629cea06da0f28
Patneedi369 <patneedi369@gmail.com>
2026-08-14 16:45:49 +0530

    Time: 0 ms (100%), Space: 19.4 MB (37.57%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -1,21 +1,24 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
-        res = [[0,0,0], [0,0,0], [0,0,0]]
+        rows = [0] * 3
+        cols = [0] * 3
+        diag = 0
+        anti_diag = 0
         
-        for i in range(len(moves)):
-            if i%2==0:
-                res[moves[i][0]][moves[i][1]] += 1
-            else:
-                res[moves[i][0]][moves[i][1]] -= 1
-
-        if [1,1,1] in res or (res[0][0]==1 and res[1][1]==1 and res[2][2]==1) or (res[0][2]==1 and res[1][1]==1 and res[2][0]==1) or (res[0][0]==1 and res[1][0]==1 and res[2][0]==1) or (res[0][1]==1 and res[1][1]==1 and res[2][1]==1) or (res[0][2]==1 and res[1][2]==1 and res[2][2]==1):
-            return "A"
-
-        elif [-1,-1,-1] in res or (res[0][0]==-1 and res[1][1]==-1 and res[2][2]==-1) or (res[0][2]==-1 and res[1][1]==-1 and res[2][0]==-1) or (res[0][0]==-1 and res[1][0]==-1 and res[2][0]==-1) or (res[0][1]==-1 and res[1][1]==-1 and res[2][1]==-1) or (res[0][2]==-1 and res[1][2]==-1 and res[2][2]==-1):
-            return "B"
-
-        elif len(moves)==9:
-            return "Draw"
-
-        else:
-            return "Pending"
+        for i, (r, c) in enumerate(moves):
+            # Player A gets +1, Player B gets -1
+            player = 1 if i % 2 == 0 else -1
+            
+            rows[r] += player
+            cols[c] += player
+            
+            if r == c:
+                diag += player
+            if r + c == 2:
+                anti_diag += player
+                
+            # If any row, col, or diagonal reaches 3 or -3, someone won!
+            if abs(rows[r]) == 3 or abs(cols[c]) == 3 or abs(diag) == 3 or abs(anti_diag) == 3:
+                return "A" if player == 1 else "B"
+                
+        return "Draw" if len(moves) == 9 else "Pending"
