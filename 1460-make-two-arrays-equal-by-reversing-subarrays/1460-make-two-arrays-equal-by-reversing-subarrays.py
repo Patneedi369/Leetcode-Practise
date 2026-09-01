3794dcd177e2338f63e8734aeb0655fcac8c9197
Lakshmi Patneedi <patneedi369@gmail.com>
2026-09-01 19:31:49 +0530

    Time: 2 ms (78.9%), Space: 19.1 MB (98.08%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
+++ 1460-make-two-arrays-equal-by-reversing-subarrays/1460-make-two-arrays-equal-by-reversing-subarrays.py
@@ -1,3 +1,12 @@
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        return Counter(target) == Counter(arr)
+        element_count = [0]*1001
+        for num in target:
+            element_count[num] += 1
+        for num in arr:
+            element_count[num] -= 1
+        
+        return all(c==0 for c in element_count)
+
+        # Time Complexity: O(N)
+        # Space Complexity: O(1) — constant extra space since the array size is fixed at 1001 regardless of input length.
