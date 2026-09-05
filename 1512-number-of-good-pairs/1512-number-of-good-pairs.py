d1cb572236d9b21c5f45ac8fbe1adfa002b8b16e
Lakshmi Patneedi <patneedi369@gmail.com>
2026-09-05 19:45:30 +0530

    Time: 0 ms (100%), Space: 19.3 MB (53.94%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -6,3 +6,16 @@ class Solution:
             res += freq[i]
             freq[i] += 1
         return res
+
+
+'''
+dry run
+
+Step,Current i,freq[i] before update,res added,Total res,freq[i] after update
+1,1,0,+ 0,0,freq[1] = 1
+2,2,0,+ 0,0,freq[2] = 1
+3,3,0,+ 0,0,freq[3] = 1
+4,1,1,+ 1,1,freq[1] = 2
+5,1,2,+ 2,3,freq[1] = 3
+6,3,1,+ 1,4,freq[3] = 2
+'''
