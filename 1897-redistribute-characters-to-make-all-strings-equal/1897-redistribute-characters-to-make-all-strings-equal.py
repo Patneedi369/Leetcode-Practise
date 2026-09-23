2cc32fbc5450eac5f68a96535369791a1c9ee2d8
Lakshmi Patneedi <patneedi369@gmail.com>
2026-09-23 19:56:34 +0530

    Time: 3 ms (97.38%), Space: 19.4 MB (64.83%) - LeetHub

---
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.py
@@ -0,0 +1,5 @@
+class Solution:
+    def makeEqual(self, words: list[str]) -> bool:
+        n = len(words)
+        counts = Counter("".join(words))
+        return all(count % n == 0 for count in counts.values())
